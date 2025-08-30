44280efa530d3b4feea44b631e6b2d4fba5bf43e
mrbokaagny <mrbokableagny@gmail.com>
2025-08-30 14:33:11 +0000

    add social networks link

--- src/core/domain/dtms/business_card.dtm.ts
+++ src/core/domain/dtms/business_card.dtm.ts
@@ -7,17 +7,19 @@ export class BusinessCardDtm {
     number : string;
     email : string;
     job : string;
+    social_networks : string[];
 
-    constructor(id : string, user : UserDtm, number : string, email : string, job : string) {
+    constructor(id : string, user : UserDtm, number : string, email : string, job : string, social_networks : string[] = []) {
         this.number = number;
         this.email = email;
         this.job = job;
         this.id = id;
         this.user = user;
+        this.social_networks = social_networks;
     }
 
     static fromBusinessCardDtm(businessCard : BusinessCardModel) : BusinessCardDtm {
         console.log(businessCard);
-        return new BusinessCardDtm(businessCard.id, UserDtm.fromUserDtm(businessCard.user), businessCard.number, businessCard.email, businessCard.job);
+        return new BusinessCardDtm(businessCard.id, UserDtm.fromUserDtm(businessCard.user), businessCard.number, businessCard.email, businessCard.job, businessCard.social_networks);
     }
 }
